b6adf5ee5af42809960276e5229ae50c5d0a53d3
raphael sena <rsenares1@gmail.com>
2025-01-30 23:20:50 -0300

    fix: fixes building

--- code/next.config.ts
+++ code/next.config.ts
@@ -3,9 +3,13 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
+  output: "export",
   basePath: isProd ? '/code' : '',
   assetPrefix: "/code",
   trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
